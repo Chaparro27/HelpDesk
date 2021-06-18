1011aebc0a129bbd5362185f983318ee206db3f0
Chaparro27 <48492306+Chaparro27@users.noreply.github.com>
2021-06-18 16:16:39 -0500

    Usuarios list

--- sistema-admin/src/views/admin/Catalogos/Usuarios.js
+++ sistema-admin/src/views/admin/Catalogos/Usuarios.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import TableCell from "@material-ui/core/TableCell";
@@ -6,7 +6,7 @@ import TableRow from "@material-ui/core/TableRow";
 
 import TableCustom from "components/Tables/TableCustom";
 import ButtonsActionsMenu from "components/Tables/ButtonsActionsMenu";
-
+import { GetClients,DeleteClients } from '../../../actions/clientesActions';
 import componentStyles from "assets/theme/views/admin/catalogos/usuarios";
 import ModalCreateUser from "components/Modal/ModalCreateUser";
 
@@ -14,125 +14,32 @@ const useStyles = makeStyles(componentStyles);
 
 const Usuarios = () => {
     const classes = useStyles();
+    const [data, setData] = React.useState([])
     const header = [
         "ID",
-        "Nombre de usaurio",
+        "Nombre de usuario",
+        "Correo"
     ];
 
-    const data = [
-        {
-            id: "1",
-            username: "efrenhhi"
-        },
-        {
-            id: "2",
-            username: "Chaparro27"
-        },
-        {
-            id: "3",
-            username: "LoopyGames"
-        },
-        {
-            id: "4",
-            username: "JoseIsiro"
-        },
-        {
-            id: "5",
-            username: "Kuriwale"
-        },
-        {
-            id: "6",
-            username: "Sh1r0"
-        },
-        {
-            id: "7",
-            username: "Chaparro27"
-        },
-        {
-            id: "8",
-            username: "LoopyGames"
-        },
-        {
-            id: "9",
-            username: "JoseIsiro"
-        },
-        {
-            id: "10",
-            username: "Kuriwale"
-        },
-        {
-            id: "11",
-            username: "Sh1r0"
-        },
-        {
-            id: "12",
-            username: "Chaparro27"
-        },
-        {
-            id: "13",
-            username: "LoopyGames"
-        },
-        {
-            id: "14",
-            username: "JoseIsiro"
-        },
-        {
-            id: "15",
-            username: "Kuriwale"
-        },
-        {
-            id: "16",
-            username: "Sh1r0"
-        },
-        {
-            id: "17",
-            username: "Chaparro27"
-        },
-        {
-            id: "18",
-            username: "LoopyGames"
-        },
-        {
-            id: "19",
-            username: "JoseIsiro"
-        },
-        {
-            id: "20",
-            username: "Kuriwale"
-        },
-        {
-            id: "21",
-            username: "Sh1r0"
-        },
-        {
-            id: "22",
-            username: "Chaparro27"
-        },
-        {
-            id: "23",
-            username: "LoopyGames"
-        },
-        {
-            id: "24",
-            username: "JoseIsiro"
-        },
-        {
-            id: "25",
-            username: "Kuriwale"
-        },
-        {
-            id: "26",
-            username: "Sh1r0"
+    useEffect(() => {
+        const fetchData = async () => {
+          const resp = await GetClients('user/');
+          setData(resp);        
         }
-    ]
+        fetchData();
+      }, []); 
+
     const body = (e) =>  
         <>
             <TableRow>
                 <TableCell classes={{ root: classes.tableCellRoot }}>
-                    {e.id}
+                    {e.idUser}
                 </TableCell>
                 <TableCell classes={{ root: classes.tableCellRoot }}>
                     {e.username}
+                </TableCell>
+                <TableCell classes={{ root: classes.tableCellRoot }}>
+                    {e.email}
                 </TableCell>
                 <ButtonsActionsMenu 
 
