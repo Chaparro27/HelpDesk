7dd23c6c23db0bd5ad738bc09b0aa96dfb51fc75
EfrenShiro <47719888+EfrenShiro@users.noreply.github.com>
2021-06-21 13:43:13 -0500

    adsfghjk

--- sistema-admin/src/views/admin/Catalogos/Tickets.js
+++ sistema-admin/src/views/admin/Catalogos/Tickets.js
@@ -4,7 +4,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 
+import { 
+  IconButton,
+} from '@material-ui/core/';
+
 import TableTickets from "components/Tables/TableTickets";
+import { useSearch } from "Hooks/useSearch";
+
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import ModalCustom from "components/Modal/ModalCustom";
+import ModalTicket from "components/Modal/ModalTicket";
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
@@ -16,6 +25,17 @@ const useStyles = makeStyles((theme) => ({
   }));
 const Tickets = () => {
 
+  const [ 
+    isSearch, 
+    search, 
+    isCreating,
+    whereUpdating,
+    handleChange,
+    reset, 
+    handleSetValues,
+    handleModalopen, 
+    handleModalClose ] = useSearch();
+
     const classes = useStyles();
     return(
         <Container 
@@ -23,11 +43,24 @@ const Tickets = () => {
           component={Box}
           marginTop="-15rem"
           className={classes.root}>
+            <IconButton
+              edge="start"
+              onClick={ handleModalopen }
+              handleModalClose ={handleModalClose}
+              className={ classes.buttons }
+              color="inherit" >
+                <AddCircleIcon  className={ classes.addicon } />
+            </IconButton>
           <Grid className={classes.paper} container spacing={3} >
               <Grid item xs={12} >
               <TableTickets/>
               </Grid>
           </Grid>
+          
+          <ModalCustom
+            handleModalClose={ handleModalClose }
+            openModal={ isCreating }
+            component={ ModalTicket } />
         </Container>
 
     );
