68e252154a5dc168516921767a73f97f04edc69a
Vladimir <rv_22n@hotmail.com>
2021-06-22 01:11:12 -0500

    Update index.js
    Update

--- sistema-admin/src/index.js
+++ sistema-admin/src/index.js
@@ -4,7 +4,6 @@ import "assets/plugins/nucleo/css/nucleo.css";
 import App from "App";
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
 ReactDOM.render(  
   <App/>,
   document.querySelector("#root")
